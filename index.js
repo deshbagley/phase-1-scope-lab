14f85abce5df898b84f450028e61b74862e43423
Desh Bagley <stemtrainer@gmail.com>
2022-03-27 15:53:41 -0400

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "larry"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return null
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return null
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return null
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = bestCustomer
+}
